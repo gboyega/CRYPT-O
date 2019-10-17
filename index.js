b482996b03af7c2d0780668a7b55386c6e16ec62
gboyega <jaiyeolagboyega@gmail.com>
2019-10-17 17:40:28 +0100

    initialised variables for dom manipulation in index.js, however, getElementById is not working.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-let request = new XMLHttpRequest();
-request.open('GET', 'https://api.coinranking.com/v1/public/coins');
-request.onload = () => {
-    var data = JSON.parse(request.responseText);
-    var coins = data.data.coins;
-    console.log(coins[0]);
-};
-request.send();
-
+let url;
+let currency;
+let gbp = document.getElementById("gbp");
+let usd = document.getElementById("usd");
+let btc = document.getElementById("btc")
 let next = document.getElementById("next");
-let previous = document.getElementById("previous");
-let content = getElementById("content");
+let back = document.getElementById("back");
+let content = document.getElementById("content");
 
-const displayCards = (data) => {
-    content.insertAdjacentHTML('beforeend')
-};
+const getData = (url) => {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = () => {
+        var data = JSON.parse(request.responseText);
+        var coins = data.data.coins;
+        console.log(coins[0]);
+    };
+    request.send();
+    return coins;
+}
 
-next.addEventListener("click", () => {
+const displayCards = (data, currency) => {
+    var card = "oriwonkoko";
+    content.insertAdjacentHTML('beforeend', card);
+}
 
-});
+// displayCards("x", "x");
+console.log(content, gbp, usd, btc, next, back);
+
+// next.addEventListener("click", () => {});
